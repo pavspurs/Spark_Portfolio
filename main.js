49e8b28cb84923f1b6697ac648c33d208eb8a2fa
S. Park <atlantis_pro@naver.com>
2021-02-02 00:20:45 +0900

    CSS: updated

--- main.js
+++ main.js
@@ -11,3 +11,26 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove("open");
+  scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
+
+const active = document.querySelector(".category__btn.selected");
+active.classList.remove("selected");
+const target = e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+e.target.classList.add("selected");
