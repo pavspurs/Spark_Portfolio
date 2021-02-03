bd254d77b8102410407ce5412efe6ca6bea54f09
S. Park <atlantis_pro@naver.com>
2021-02-03 21:09:28 +0900

    JS: Projects Filtering

--- main.js
+++ main.js
@@ -77,15 +77,18 @@ workBtnContainer.addEventListener("click", (e) => {
   if (filter == null) {
     return;
   }
-  console.log(filter);
-  projects.forEach((project) => {
-    console.log(project.dataset.type);
-    if (filter === "*" || filter === project.dataset.type) {
-      project.classList.remove("invisible");
-    } else {
-      project.classList.add("invisible");
-    }
-  });
+  projectContainer.classList.add("animation-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("animation-out");
+  }, 300);
 });
 
 // Navbar toggle button for small screen
